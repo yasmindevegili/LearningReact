18ee61d49a91639b090e096308f8ec05325402a1
Yasmin Devegili <48127784+yasmindevegili@users.noreply.github.com>
2023-06-01 16:29:02 -0300

    Repositório inicial para testes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import ReactDom from "react-dom";
+import "./index.css";
+import Welcome from "./components/basics/welcome";
+ReactDom.render(
+  <div id='app'>
+    <Welcome />
+  </div>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
